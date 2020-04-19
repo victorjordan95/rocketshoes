788f56935d595f8a981f03a6bf0b24b374ea4a51
Victor Jordan <sistema.sgo.ccb@gmail.com>
2020-04-19 16:55:47 -0300

    adding redux files config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
+
+import GlobalStyle from './styles/global';
+
+import Header from './components/Header';
+import Routes from './routes';
+
+import store from './store';
 
 function App() {
-  return <h1>Rocketshoes</h1>;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
