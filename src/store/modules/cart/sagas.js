a858e32eaf2e171212e2b58805501a7747ebc6ac
Victor Jordan <sistema.sgo.ccb@gmail.com>
2020-05-21 09:06:29 -0300

    Finishing rocketshoes app

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,16 +1,16 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify';
 
-import api from '../../../services/api'
-import { formatPrice } from '../../../utils/format'
+import api from '../../../services/api';
+import history from '../../../services/history';
+import { formatPrice } from '../../../utils/format';
 
-import { addToCartSuccess, updateAmount } from './actions'
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
-
-  const productExists = yield select(
-    state => state.cart.find(p => p.id === id),
-  )
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
 
   const stock = yield call(api.get, `/stock/${id}`);
 
@@ -20,27 +20,41 @@ function* addToCart({ id }) {
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-    toast.error('Quantidade solicitada fora do estoque')
-    return
+    toast.error('Quantidade solicitada fora do estoque');
+    return;
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount))
-
+    yield put(updateAmountSuccess(id, amount));
   } else {
-      const response = yield call(api.get, `/products/${id}`)
+    const response = yield call(api.get, `/products/${id}`);
 
-      const data = {
-        ...response.data,
-        amount: 1,
-        priceFormatted: formatPrice(response.data.price)
-      }
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
 
-      yield put(addToCartSuccess(data))
-
+    yield put(addToCartSuccess(data));
+    history.push('/cart');
   }
+}
+
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora do estoque');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
 }
 
 export default all([
   takeLatest('@cart/ADD_REQUEST', addToCart),
-])
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
